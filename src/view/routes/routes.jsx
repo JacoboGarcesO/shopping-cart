3b95dcf0079e93b5b51d91705b8d8617074ba9c8
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2023-11-09 08:19:16 -0500

    Deploy

--- src/view/routes/routes.jsx
+++ src/view/routes/routes.jsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import { Home } from '../pages/Home.jsx'
 import { Admin } from '../pages/Admin.jsx'
 import { Layout } from '../components/Layout/Layout.jsx'
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: '/',
     Component: Layout,
